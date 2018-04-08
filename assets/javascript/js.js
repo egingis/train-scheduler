ac496c9ec2d2c8f486ece215a72748e6b9f42f71
egingis <33986417+egingis@users.noreply.github.com>
2018-04-08 18:25:34 -0400

    Add files via upload

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,15 +1,6 @@
-var config = {
-    apiKey: "AIzaSyASl5S3sVA4avdUaIkiOUU8-xJn0GiOkbg",
-    authDomain: "trainscheduler-9f045.firebaseapp.com",
-    databaseURL: "https://trainscheduler-9f045.firebaseio.com/",
-    storageBucket: "trainscheduler-9f045.appspot.com"
-  };
-  
-  firebase.initializeApp(config);
-
 $(document).ready(function(){
 	// 1. Link to Firebase
-	var trainData = firebase.database("https://trainscheduler-9f045.firebaseio.com/");
+	var trainData = firebase.database();
 
 
 	// 2. Button for adding Trains
@@ -17,14 +8,12 @@ $(document).ready(function(){
 
 		// Grabs user input and assign to variables
 		var trainName = $("#trainNameInput").val().trim();
-		var lineName = $("#lineInput").val().trim();
 		var destination = $("#destinationInput").val().trim();
 		var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(10, "years").format("X");;
 		var frequencyInput = $("#frequencyInput").val().trim();
 
 		// Test for variables entered
 		console.log(trainName);
-		console.log(lineName);
 		console.log(destination);
 		console.log(trainTimeInput);
 		console.log(frequencyInput);
@@ -33,7 +22,6 @@ $(document).ready(function(){
 		// Will push this to firebase
 		var newTrain = {
 			name:  trainName,
-			line: lineName,
 			destination: destination,
 			trainTime: trainTimeInput,
 			frequency: frequencyInput,
@@ -44,7 +32,6 @@ $(document).ready(function(){
 
 		// clear text-boxes
 		$("#trainNameInput").val("");
-		$("#lineInput").val("");
 		$("#destinationInput").val("");
 		$("#trainInput").val("");
 		$("#frequencyInput").val("");
@@ -59,7 +46,6 @@ $(document).ready(function(){
 
 		// assign firebase variables to snapshots.
 		var firebaseName = childSnapshot.val().name;
-		var firebaseLine = childSnapshot.val().line;
 		var firebaseDestination = childSnapshot.val().destination;
 		var firebaseTrainTimeInput = childSnapshot.val().trainTime;
 		var firebaseFrequency = childSnapshot.val().frequency;
